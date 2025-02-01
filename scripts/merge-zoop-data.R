8bad710beaeca7e44c6fa91f19715ed4efe59dc1
LiuqianBao0517 <129997013+LiuqianBao0517@users.noreply.github.com>
2025-02-01 14:36:12 -0800

    merge zoop data

---
+++ scripts/merge-zoop-data.R
@@ -0,0 +1,11 @@
+# Access the zooplankton data
+library(readr)
+calcofi_zoop_data <- read_csv("~/Documents/PSTAT197B/data/195101-201607_1701-1704_1802-1804_Zoop.csv")
+
+# merge the zoop data with the occan acdification data
+merged_zoop_data <- inner_join(
+  oah_bottle, 
+  calcofi_zoop_data,
+  by = join_by(Date == Tow_Date, Station_ID == Sta_ID)
+)
+
